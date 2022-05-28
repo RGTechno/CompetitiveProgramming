ec31cd0a1460137961abf571b09e039ba5f38392
Rahul Gandhi <65856344+RGTechno@users.noreply.github.com>
2022-05-28 19:17:40 +0530

    .

--- AtCoder/Beg Contest 253/D_-_FizzBuzz_Sum_Hard.cpp
+++ AtCoder/Beg Contest 253/D_-_FizzBuzz_Sum_Hard.cpp
@@ -1,72 +1,41 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+ 
 #define endl "\n"
 #define int long long int
 #define mod 1000000007
-
+ 
 int gcd(int a, int b)
 {
     if (b != 0)
         return gcd(b, a % b);
     return a;
 }
-
+ 
 void solve()
 {
     int n, a, b;
     cin >> n >> a >> b;
-
+ 
     set<int> st;
-
+ 
     int total = (n * (n + 1)) / 2;
-
+ 
     int gcdAB = gcd(a, b);
-
+ 
+    int sum_of_multiple_of_A = (a * (((n / a) * ((n / a) + 1)) / 2));
+    int sum_of_multiple_of_B = (b * (((n / b) * ((n / b) + 1)) / 2));
+ 
     int lcm = (a * b) / gcdAB;
-
-    int i = lcm;
-    while (i <= n)
-    {
-        if (i % lcm == 0)
-        {
-            if (st.count(i) == 0)
-            {
-                total -= i;
-            }
-        }
-        i += lcm;
-    }
-
-    i = a;
-    while (i <= n)
-    {
-        if (i % a == 0)
-        {
-            if (st.count(i) == 0)
-            {
-                total -= i;
-            }
-        }
-        i += a;
-    }
-
-    i = b;
-    while (i <= n)
-    {
-        if (i % b == 0)
-        {
-            if (st.count(i) == 0)
-            {
-                total -= i;
-            }
-        }
-        i += b;
-    }
-
-    cout << total << endl;
+    int sum_of_multiple_of_both_AandB = (lcm * (((n / lcm) * ((n / lcm) + 1)) / 2));
+ 
+    int y = (sum_of_multiple_of_A + sum_of_multiple_of_B);
+    int x = y - sum_of_multiple_of_both_AandB;
+ 
+    int ans = total - x;
+    cout << ans << endl;
 }
-
+ 
 int32_t main()
 {
     ios_base::sync_with_stdio(false);
